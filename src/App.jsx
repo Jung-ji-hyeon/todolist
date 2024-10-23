6672ca86b0efb94f484c0939adfc567edbfa22f3
Jung-ji-hyeon <ejxldortm@naver.com>
2024-10-23 17:47:33 +0900

    임시 데이터 및 기능 구현 준비

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getDate(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "청소하기",
+    date: new Date().getDate(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getDate(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState([mockData]);
   return (
     <div className="App">
       <Header />
